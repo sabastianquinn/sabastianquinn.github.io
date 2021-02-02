3ca65924b3d100405d6f8dff2420474956387dce
sabastianquinn <77514231+sabastianquinn@users.noreply.github.com>
2021-02-02 14:26:38 -0500

    Create login.php

---
+++ My Website/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+<title> Login</login>
+</head>
+<body>
+<form>
+<label> Username:</label>
+<input type="text" name="user" />
+<br>
+<label>Password:</label>
+<input type="text" name="pass"/>
+<br>
+<input type="submit" name="login"/>
